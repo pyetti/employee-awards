24d20255cc7e603433b750f816e4c7c45a8f5736
pyetti <payetti@gmail.com>
2018-07-08 18:01:47 -0400

    Connecting to Cloud SQL locally and from App Engine

--- modules/dbConfig.js
+++ modules/dbConfig.js
@@ -3,7 +3,7 @@ let mysql = require('mysql');
 module.exports = {
 	pool: mysql.createPool({
 				connectionLimit	: 10,
-				host  			: '35.196.130.143',
+				socketPath    		: '/cloudsql/windy-smoke-209201:us-east1:employee-rewards', // '35.196.130.143',
 				user  			: 'root',
 				password		: '25ptisGaAOdgBn3C',
 				database		: 'employee_rewards'
