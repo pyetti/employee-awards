f5e44927079932c9be11a2bdb1eba02f8e8785b2
Peter Yetti <pyetti@users.noreply.github.com>
2018-07-28 13:29:30 -0400

    Checking token not undefined before checking length; sending correct error message for failed login

--- modules/auth/routes.js
+++ modules/auth/routes.js
@@ -6,8 +6,11 @@ module.exports = router;
 
 router.post('/login', (request, response) => {
     login.login(request, (err, results) => {
-        if (err || results.token.length === 0) {
-            response.sendStatus(403);
+        if (err || !results.token || results.token.length === 0) {
+            response.status(403);
+            response.json({
+                message: "Invalid credentials"
+            })
         } else {
             response.status(200);
             response.json({
