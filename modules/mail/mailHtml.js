b3763ced5be9bfdec56841fd314140c9a8e42e11
Peter Yetti <pyetti@users.noreply.github.com>
2018-08-09 14:19:25 -0700

    Replaced localhost with backend URL

--- modules/mail/mailHtml.js
+++ modules/mail/mailHtml.js
@@ -11,7 +11,7 @@ function passwordRecoveryEmail(encryptedEmail, token) {
         <p>Click the button below to update your password. The link is only available to 24 hours and can be used only once.</p>
         <br>
         <a style="padding: 10px; background-color: #b56969; border: none; border-radius: 5px; margin: 10px; color: #e8edf3; text-decoration: none;"
-         href="http://localhost:8081/updatePassword?e=${encryptedEmail}&t=${token}">Update Password</a>
+         href="https://employee-rewards-front-end.appspot.com/#/updatePassword?e=${encryptedEmail}&t=${token}">Update Password</a>
       </div>
     `;
 }
